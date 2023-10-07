edbfdea751e4194c9c2af7ea2ab850f823f6f07f
ga28299 <100487946+ga28299@users.noreply.github.com>
2023-10-07 13:53:24 -0400

    Added handler

--- main.go
+++ main.go
@@ -1,17 +1,49 @@
 package main
 
 import (
-	"fmt"
 	"log"
+	"net/http"
 	"os"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/cors"
+	"github.com/joho/godotenv"
 )
 
 func main() {
-	fmt.Println("hello world!")
+
+	godotenv.Load(".env")
 
 	portString := os.Getenv("PORT")
 	if portString == "" {
 		log.Fatal("No port set. Check env file")
 	}
-	fmt.Println("port is set to", portString)
+
+	router := chi.NewRouter()
+	router.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{"https://*", "http://*"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"*"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: false,
+		MaxAge:           300, // Maximum value not ignored by any of major browsers
+	}))
+
+	v1Router := chi.NewRouter()
+	v1Router.Get("/healthz", handlerReadiness)
+	v1Router.Get("/err", handlerErr)
+
+	router.Mount("/v1", v1Router)
+
+	srv := &http.Server{
+		Handler: router,
+		Addr:    ":" + portString,
+	}
+
+	log.Printf("Server starting on Port %s", portString)
+	err := srv.ListenAndServe()
+	if err != nil {
+		log.Fatal(err)
+	}
+
 }
